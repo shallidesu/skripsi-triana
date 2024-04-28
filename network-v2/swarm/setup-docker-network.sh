ef608a757639e068befe4f40bd1e6d78ab17d41f
shallidesu <shallitriana@gmail.com>
2024-04-28 16:36:09 +0700

    setup-swarm-network.sh

--- network-v2/swarm/setup-docker-network.sh
+++ network-v2/swarm/setup-docker-network.sh
@@ -12,6 +12,8 @@ clear
 # ---------------------------------------------------------------------------
 docker node update --label-add name=manager $NODE1_ID
 docker node update --label-add name=worker1 $NODE2_ID
+docker inspect $NODE1_ID
+docker inspect $NODE2_ID
 
 # ---------------------------------------------------------------------------
 # Create a network
