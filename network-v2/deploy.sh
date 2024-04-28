1bb46d8acf99c709591cafdd748f6a903ccea2e4
shallidesu <shallitriana@gmail.com>
2024-04-28 19:18:07 +0700

    docker-corrected

--- network-v2/deploy.sh
+++ network-v2/deploy.sh
@@ -9,6 +9,6 @@ echo
 echo "# ---------------------------------------------------------------------------"
 echo "# Deploy Nodes"
 echo "# ---------------------------------------------------------------------------"
-    docker stack deploy -c  docker/docker-compose-network.yaml -c  docker/docker-compose-couch.yaml -c  docker/docker-compose-cli.yaml hlf 2>&1
+    docker stack deploy -c docker/docker-compose-network.yaml -c docker/docker-compose-couch.yaml -c docker/docker-compose-cli.yaml hlf 2>&1
     docker ps -a
 sleep 5
