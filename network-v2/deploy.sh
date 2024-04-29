f9c9499a62258f2115f2ec5e8d63abced3d7e280
shallidesu <shallitriana@gmail.com>
2024-04-29 10:45:04 +0700

    network dan network-v2

--- network-v2/deploy.sh
+++ network-v2/deploy.sh
@@ -10,6 +10,7 @@ echo "# ------------------------------------------------------------------------
 echo "# Deploy Nodes"
 echo "# ---------------------------------------------------------------------------"
     docker stack deploy -c docker/docker-compose-network.yaml -c docker/docker-compose-couch.yaml -c docker/docker-compose-cli.yaml hlf 2>&1
-    sleep 5
+    sleep 10
     docker ps -a
+    sleep 10
     docker service ls
--- network-v2/run.sh
+++ network-v2/run.sh
@@ -8,50 +8,6 @@
 # ---------------------------------------------------------------------------
 clear
 
-# # ---------------------------------------------------------------------------
-# # Generating CA
-# # ---------------------------------------------------------------------------
-# function createOrgs() {
-#     if [ -d "organizations/peerOrganizations" ]; then
-#         echo "# ---------------------------------------------------------------------------"
-#         echo "# Remove old artifacts"
-#         echo "# ---------------------------------------------------------------------------"
-#         rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
-#     fi
-
-#     # ---------------------------------------------------------------------------
-#     # Create crypto material using Fabric CA
-#     # ---------------------------------------------------------------------------
-#     echo 
-#     echo "# ---------------------------------------------------------------------------"
-#     echo "# Generating certificates using Fabric CA"
-#     echo "# ---------------------------------------------------------------------------"
-#     docker stack deploy -c $COMPOSE_FILE_CA hlf 2>&1
-#     . organizations/fabric-ca/registerEnroll.sh
-
-#     while :
-#     do
-#         if [ ! -f "organizations/fabric-ca/pemilihan/tls-cert.pem" ]; then
-#         sleep 1
-#         else
-#         break
-#         fi
-#     done
-
-#     echo
-#     echo "# ---------------------------------------------------------------------------"
-#     echo "# Creating Pemilihan Identities"
-#     createPemilihan
-#     echo "# ---------------------------------------------------------------------------"
-#     echo "# Creating Orderer Org Identities"
-#     createOrderer
-
-#     echo
-#     echo "# ---------------------------------------------------------------------------"
-#     echo "# Generating CCP files for Pemilihan"
-#     ./organizations/ccp-generate.sh
-# }
-
 # ---------------------------------------------------------------------------
 # Up network
 # ---------------------------------------------------------------------------
