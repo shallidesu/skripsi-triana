77f2be247ff2da8b7a1e0231395e742ef7b8a1f5
shallidesu <shallitriana@gmail.com>
2024-05-02 12:35:47 +0700

    network revisi docker dan deploy

--- network/deploy.sh
+++ network/deploy.sh
@@ -9,7 +9,7 @@ echo
 echo "# ---------------------------------------------------------------------------"
 echo "# Deploy Nodes"
 echo "# ---------------------------------------------------------------------------"
-    docker stack deploy -c docker/docker-compose-network.yaml -c docker/docker-compose-couch.yaml -c docker/docker-compose-cli.yaml hlf 2>&1
+    docker stack deploy -c docker/docker-compose-network.yaml -c docker/docker-compose-couch.yaml -c docker/docker-compose-cli.yaml hlf --with-registry-auth 2>&1
     sleep 10
     docker ps -a
     sleep 10
