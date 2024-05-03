77d26a33a77dfcbc6b8587fb99dbbdaa50ef4a73
shallidesu <shallitriana@gmail.com>
2024-05-03 21:51:22 +0700

    deploy.sh dan deployCC.sh

--- network/deploy.sh
+++ network/deploy.sh
@@ -10,7 +10,5 @@ echo "# ------------------------------------------------------------------------
 echo "# Deploy Nodes"
 echo "# ---------------------------------------------------------------------------"
     docker stack deploy -c docker/docker-compose-network.yaml -c docker/docker-compose-couch.yaml -c docker/docker-compose-cli.yaml hlf --with-registry-auth 2>&1
-    sleep 10
-    docker ps -a
-    sleep 10
+    docker ps
     docker service ls
